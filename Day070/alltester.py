7b78fbebb61cb590d272f156fb41cf9e036039e7
Anishcharith <anish.charith@gmail.com>
2018-02-14 19:50:11 +0530

    model based on smart money and crowd money

---
+++ Day070/alltester.py
@@ -0,0 +1,23 @@
+import os
+import backtester
+import sys
+import pandas as pd
+days=sys.argv[1]
+
+def main(days):
+    NIFTY=pd.read_csv('ind_nifty50list.csv')
+    NIFTYnse=NIFTY['Symbol'].values
+    f=open(str(days)+'nifty50.txt','w')
+    sumi=0
+    totaldaysused=0
+    for comp in NIFTYnse:
+        result,daysused=backtester.main(comp,int(days))
+        f.write(comp+' : '+str(result)+'\n')
+        sumi+=result
+        totaldaysused+=daysused
+    print(sumi/50)
+    print(totaldaysused/50)
+    f.close()
+
+if __name__=="__main__":
+    main(days)
