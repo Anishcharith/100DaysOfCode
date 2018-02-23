fd7cb31c1bb0a83477b3497b4957ceffa9f72272
Anishcharith <anish.charith@gmail.com>
2018-02-23 21:56:41 +0530

    default period

--- Day034/Qstick.py
+++ Day034/Qstick.py
@@ -2,7 +2,7 @@ import sys
 sys.path.insert(0,'../Day002/')
 from EMA import EMA
 
-def Qstick(closep,openp,period):
+def Qstick(closep,openp,period=8):
     diff=[i-j for (i,j) in zip(closep,openp)]
     Qstick=EMA(diff,period)
     return Qstick
