489fd5fdbf56f6e1ab32732ae80197686cbd0e13
Anishcharith <anish.charith@gmail.com>
2018-02-26 20:32:37 +0530

    Model based on MI and Supertrend

---
+++ Day081/model.py
@@ -0,0 +1,59 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+# Technical indicators
+sys.path.insert(0,'../Day031/')
+sys.path.insert(0,'../Day040/')
+from MassIndex import MassIndex
+from supertrend import supertrend 
+# --------------------
+
+def buy(open_p,close_p,low_p,high_p,volume):
+    buy=[0]
+    mi=MassIndex(high_p,low_p)
+    st=supertrend(close_p,high_p,low_p)
+    for i in range(1,len(close_p)):
+        if mi[i]>=25 and st[i]<close_p[i]:
+            buy.append(1)
+        else:
+            buy.append(0)
+    return buy
+
+def sell(open_p,close_p,low_p,high_p,volume):
+    sell=[0]
+    mi=MassIndex(high_p,low_p)
+    for i in range(1,len(close_p)):
+        if mi[i]>27:
+            sell.append(1)
+        else:
+            sell.append(0)
+    return sell
+
+def stoploss(open_p,close_p,low_p,high_p,volume):
+    stoploss=[0]
+    mi=MassIndex(high_p,low_p)
+    for i in range(1,len(close_p)):
+        if mi[i]<25:
+            stoploss.append(1)
+        else:
+            stoploss.append(0)
+    return stoploss
+
+def main(comp,days=None,start=0):
+    data=pd.read_csv('../Data/'+comp+'.csv')
+    if days==None:
+        days=len(data.index)
+    close_p=np.flipud(data['Close'].values[start:start+days])
+    open_p=np.flipud(data['Open'].values[start:start+days])
+    low_p=np.flipud(data['Low'].values[start:start+days])
+    high_p=np.flipud(data['High'].values[start:start+days])
+    volume=np.flipud(data['Total Trade Quantity'].values[start:start+days])
+    buy_a=buy(open_p,close_p,low_p,high_p,volume)
+    sell_a=sell(open_p,close_p,low_p,high_p,volume)
+    stoploss_a=stoploss(open_p,close_p,low_p,high_p,volume)
+    return buy_a,sell_a,stoploss_a
+
+if __name__=="__main__":
+    comp=sys.argv[1]
+    main(comp)
