197ba3667cb3895684575a9a78919b3b777821b2
Anishcharith <anish.charith@gmail.com>
2018-03-13 23:51:11 +0530

    smallcap

--- template/getnifty50.py
+++ template/getnifty50.py
@@ -2,12 +2,13 @@ import urllib.request
 import pandas as pd
 import os
 tryagain=[]
-NIFTY=pd.read_csv('ind_nifty50list.csv')
+#NIFTY=pd.read_csv('ind_nifty50list.csv')
+NIFTY=pd.read_csv('ind_niftysmallcap250list.csv')
 NIFTYnse=NIFTY['Symbol'].values
 os.system('mkdir -p ../Data/')
 for comp in NIFTYnse:
     try:
-        urllib.request.urlretrieve('https://www.quandl.com/api/v3/datasets/NSE/'+comp+'.csv',filename='../Data/'+comp+'.csv')
+        urllib.request.urlretrieve('https://www.quandl.com/api/v3/datasets/NSE/'+comp+'.csv?api_key=w35G1yonhySwCP3dyoDj',filename='../Data/'+comp+'.csv')
         print(comp+' done')
     except:
         tryagain.append(comp);
@@ -17,7 +18,7 @@ while(tryagain!=[]):
     print("trying again")
     for comp in tryagain:
         try:
-            urllib.request.urlretrieve('https://www.quandl.com/api/v3/datasets/NSE/'+comp+'.csv',filename='../Data/'+comp+'.csv')
+            urllib.request.urlretrieve('https://www.quandl.com/api/v3/datasets/NSE/'+comp+'.csv?api_key=w35G1yonhySwCP3dyoDj',filename='../Data/'+comp+'.csv')
             print(comp+' done')
         except:
             temp.append(comp);
